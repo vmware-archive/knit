ac5ba8f99c57dd4942849ad70a30321bb064946b
Ryan Moran <ryanmoran@users.noreply.github.com>
2016-09-17 17:04:00 -0700

    Adds git version checker to ensure version >= 2.9.0 (#6)

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/pivotal-cf-experimental/knit/patcher"
 )
@@ -48,18 +52,23 @@ func main() {
 	}
 
 	if missingFlag != "" {
-		log.Fatalln(missingFlag)
+		log.Fatal(missingFlag)
 	}
 
 	gitPath, err := exec.LookPath("git")
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
 	versionsParser := patcher.NewVersionsParser(version, patcher.NewPatchSet(patchesRepository))
 	runner, err := patcher.NewCommandRunner(gitPath, quiet)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
+	}
+
+	err = checkGitVersion(runner)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	repo := patcher.NewRepo(runner, releaseRepository, "bot", "witchcraft@example.com")
@@ -67,11 +76,46 @@ func main() {
 
 	initialCheckpoint, err := versionsParser.GetCheckpoint()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
 	err = apply.Checkpoint(initialCheckpoint)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
+}
+
+func checkGitVersion(runner patcher.CommandRunner) error {
+	out, err := runner.CombinedOutput(patcher.Command{
+		Args: []string{"--version"},
+	})
+	matches := regexp.MustCompile(`.*(\d\.\d\.\d).*`).FindStringSubmatch(string(out))
+	if len(matches) < 2 {
+		return errors.New("could not determine `git` version")
+	}
+
+	parts := strings.Split(matches[1], ".")
+	if len(parts) < 3 {
+		return errors.New("could not determine `git` version")
+	}
+
+	major, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return fmt.Errorf("could not determine `git` version: %s", err)
+	}
+
+	minor, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return fmt.Errorf("could not determine `git` version: %s", err)
+	}
+
+	if major < 2 {
+		return errors.New("knit requires a version of git >= 2.9.0")
+	}
+
+	if major == 2 && minor < 9 {
+		return errors.New("knit requires a version of git >= 2.9.0")
+	}
+
+	return nil
 }
